1cfdca6a8f66e4241bb57d6b39079cbf9387e79a
ppaulis <ppaulis@gmail.com>
2016-11-02 20:00:16 +0100

    working on DB setup via Phing

--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
-
-BUILD=$1
-
-cd /var/www/continuousphp/$BUILD
+cd /var/www/continuousphp/current
 
 while [ ! -f /var/lib/continuousphp/credentials.ini ]
 do
   sleep 2
 done
 
-chown -R www-data:www-data /var/www/continuousphp/$BUILD/*
+chown -R www-data:www-data /var/www/continuousphp/current/*
 bin/phing -propertyfile /var/lib/continuousphp/credentials.ini init
