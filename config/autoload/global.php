f705653802e4981dcfc3336878f4be24fbdc3ab0
Thomas Metois <thomas@netlor.fr>
2016-11-07 16:41:36 +0100

    Cache zf conf & use doctrine cache service

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -14,40 +14,7 @@ return array(
     // DOCTRINE CONF
     'service_manager' => array(
         'factories' => array(
-            'doctrine.cache.mycache' => function(\Zend\ServiceManager\ServiceManager $sm){
-                $arrayCache = new \Doctrine\Common\Cache\ArrayCache();
-
-                if(getenv('APPLICATION_ENV') == 'production'){
-                    if(extension_loaded('apc')){
-                        $apcCache = new \Doctrine\Common\Cache\ApcCache();
-                        $chainCache = new \Doctrine\Common\Cache\ChainCache([$apcCache,$arrayCache]);
-                        return $chainCache;
-                    }elseif(extension_loaded('apcu')){
-                        $apcuCache = new \Doctrine\Common\Cache\ApcuCache();
-                        $chainCache = new \Doctrine\Common\Cache\ChainCache([$apcuCache,$arrayCache]);
-                        return $chainCache;
-                    }
-                    // TODO: untested / add param for memchache(d) host & port
-                    /*elseif(extension_loaded('memcache')){
-                        $memcache = new \Memcache();
-                        if($memcache->connect('localhost', 11211)){
-                            $cache = new \Doctrine\Common\Cache\MemcacheCache();
-                            $cache->setMemcache($mem);
-                            $chainCache = new \Doctrine\Common\Cache\ChainCache([$cache,$arrayCache]);
-                            return $chainCache;
-                        }
-                    }elseif(extension_loaded('memcached')){
-                        $memcache = new \Memcached();
-                        if($memcache->connect('localhost', 11211)){
-                            $cache = new \Doctrine\Common\Cache\MemcachedCache();
-                            $cache->setMemcached($mem);
-                            $chainCache = new \Doctrine\Common\Cache\ChainCache([$cache,$arrayCache]);
-                            return $chainCache;
-                        }
-                    }*/
-                }
-                return $arrayCache;
-            },
+            'doctrine.cache.mycache' => 'MonarcCore\Service\DoctrineCacheServiceFactory',
         ),
     ),
     'doctrine' => array(
