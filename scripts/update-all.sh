ef26f7e81120f9ea2767c1919d35c6ba8aaff46a
Cyril Rouyer <cyril.rouyer@gmail.com>
2017-03-10 13:40:45 +0100

    EVO - update-all.sh : remove FO part in MonarcAppBO

--- scripts/update-all.sh
+++ scripts/update-all.sh
@@ -8,10 +8,21 @@ pull_if_exists() {
 	fi
 }
 
-
 git pull
 
-php composer.phar update -o
+composercommand=`command -v composer`
+if [[ -z "$composercommand" ]]; then
+	if [[ ! -f "composer.phar" ]]; then
+		# https://getcomposer.org/download/
+		php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+		php -r "if (hash_file('SHA384', 'composer-setup.php') === '669656bab3166a7aff8a7506b8cb2d1c292f042046c5a994c43155c0be6190fa0355160742ab2e1c88d40d5be660b410') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+		php composer-setup.php
+		php -r "unlink('composer-setup.php');"
+	fi
+	php composer.phar update -o
+else
+	$composercommand update -o
+fi
 
 currentPath=`pwd`
 pathCore="module/MonarcCore"
@@ -25,12 +36,6 @@ if [ -d $pathBO ]; then
 	pull_if_exists $pathBO
 else
 	pathBO="vendor/monarc/backoffice"
-fi
-pathFO="module/MonarcFO"
-if [ -d $pathFO ]; then
-	pull_if_exists $pathFO
-else
-	pathFO="vendor/monarc/frontoffice"
 fi
 
 if [[ -d node_modules && -d node_modules/ng_anr ]]; then
@@ -61,17 +66,6 @@ if [ -d $pathBO ]; then
 
 	if [ -d "${pathBO}/hooks" ]; then
 		cd $pathBO/.git/hooks
-		ln -s ../../hooks/pre-commit.sh pre-commit 2>/dev/null
-		chmod u+x pre-commit
-		cd $currentPath
-	fi
-fi
-
-if [ -d $pathFO ]; then
-	php ./vendor/robmorgan/phinx/bin/phinx migrate -c ./$pathFO/migrations/phinx.php
-
-	if [ -d "$pathFO/hooks" ]; then
-		cd $pathFO/.git/hooks
 		ln -s ../../hooks/pre-commit.sh pre-commit 2>/dev/null
 		chmod u+x pre-commit
 		cd $currentPath
